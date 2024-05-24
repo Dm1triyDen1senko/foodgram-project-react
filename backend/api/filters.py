3a60917cf7b64c01b4486dec7c501de695ad54d7
Dmitriy Denisenko <denisenkodmi@mail.ru>
2024-05-24 22:54:07 +0300

    More on Revision 5 - More on Adding 2 or more recipes to the Shopping List is now available

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -17,20 +17,9 @@ class RecipeFilter(FilterSet):
         to_field_name='slug',
         queryset=models.Tag.objects.all(),
     )
-    is_favorited = filters.BooleanFilter(method='favorited')
-    is_in_shopping_cart = filters.BooleanFilter(method='in_cart')
+    is_favorited = filters.BooleanFilter(field_name='is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(field_name='is_in_shopping_cart')
 
     class Meta:
         model = models.Recipe
         fields = ('tags', 'author')
-
-    def favorited(self, queryset, name, value):
-        if value is not None and self.request.user.is_authenticated:
-            favorites = models.Favorites.objects.get(author=self.request.user)
-            return queryset.filter(favorites=favorites)
-        return queryset
-
-    def in_cart(self, queryset, name, value):
-        if value and not self.request.user.is_anonymous:
-            return queryset.filter(author=self.request.user)
-        return queryset
